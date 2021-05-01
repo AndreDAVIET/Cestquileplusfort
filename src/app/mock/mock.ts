a17bee2edcd900cfad92a6f13319dc06c49ae333
AndreDAVIET <andredaviet@hotmail.fr>
2021-05-01 11:55:24 +0200

    homepage

---
+++ src/app/mock/mock.ts
@@ -0,0 +1,52 @@
+export let playeurs = [
+    {
+        "name" : "Jean",
+        "url" : "titi",
+        "score" : 1,
+    },
+    {
+        "name" : "Fold",
+        "url" : "toto",
+        "score" : 10,
+    },
+    {
+        "name" : "Jean",
+        "url" : "titi",
+        "score" : 1,
+    },
+    {
+        "name" : "Fold",
+        "url" : "toto",
+        "score" : 10,
+    },
+    {
+        "name" : "Jean",
+        "url" : "titi",
+        "score" : 1,
+    },
+    {
+        "name" : "Fold",
+        "url" : "toto",
+        "score" : 10,
+    },
+    {
+        "name" : "Lodl",
+        "url" : "tutu",
+        "score" : 23,
+    },
+    {
+        "name" : "Dert",
+        "url" : "tutu",
+        "score" : 52,
+    },
+    {
+        "name" : "Frtg",
+        "url" : "tutu",
+        "score" : 353,
+    },
+    {
+        "name" : "Dsez",
+        "url" : "tutu",
+        "score" : 155,
+    },
+]
---
+++ src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { playeurs } from 'src/app/mock/mock';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  lists = playeurs;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.sortList();
+  }
+
+  sortList (){
+    this.lists.sort(function (a, b) {
+      return b.score - a.score;
+   });
+  }
+
+}
